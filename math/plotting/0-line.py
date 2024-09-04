50f9da4260b9164c6ae7ff3882400be8599a8c2f
lola <f.akinwande@alustudent.com>
2024-09-04 16:55:55 +0200

    Plotting a line graph for Y axis

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,6 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+This script generates a line graph where the y values are the cubed values of x.
+The x values range from 0 to 10, and the y values are calculated as y = x^3.
+The data is plotted as a solid red line, and the graph is labeled accordingly.
+
 # Define x values (range from 0 to 10)
 x = np.arange(0, 11)
 
